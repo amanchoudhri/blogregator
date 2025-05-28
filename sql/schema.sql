ad37c5626698b72f89ff42c7a94f0666e53c6e88
Aman Choudhri <amanchoudhri@gmail.com>
2025-05-28 10:54:09 -0400

    Fix schema, previous file was outdated

--- sql/schema.sql
+++ sql/schema.sql
@@ -13,12 +13,23 @@ CREATE TABLE IF NOT EXISTS posts (
     blog_id INTEGER REFERENCES blogs(id),
     title TEXT,
     url TEXT UNIQUE,
-    topics TEXT,
     reading_time INTEGER,
     summary TEXT,
     publication_date TIMESTAMPTZ,
     discovered_date TIMESTAMPTZ DEFAULT NOW()
 );
+
+CREATE TABLE topics (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE post_topics (
+    post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+    topic_id INTEGER REFERENCES topics(id) ON DELETE CASCADE,
+    PRIMARY KEY (post_id, topic_id)
+);
+
 CREATE TABLE IF NOT EXISTS error_log (
     id SERIAL PRIMARY KEY,
     blog_id INTEGER REFERENCES blogs(id),
